3394eecf0e6105e129b9c55ec487b18b0065d01d
Sicheng Hao <sichenghao7654049862@gmail.com>
2020-10-13 13:46:27 -0400

    Add Figure

---
+++ analysis/plot_output.R
@@ -0,0 +1,33 @@
+
+setwd("/Users/sichenghao/Documents/GitHub/sodium_gcs_mortality/analysis/")
+
+png("Figure.png", width = 10, height = 4, units = 'in', res = 1000)
+ggplot()+
+  geom_area(data=mediation_results_propmediated,aes(x=cat,y=Estimate*100,fill=results))+
+  xlab('Baseline Sodium Category')+
+  ylab('%')+
+  # scale_color_manual(labels = c("Proportion Mediated"), values = c("#f1c40f"))+
+  scale_fill_manual(labels = c("Proportion Mediated"), values = c("#f1c40f"))+
+  labs(colour='Mortality Risk', fill='Proportion Mediated') +
+  theme(legend.position="bottom")+
+  theme_minimal()+
+  scale_x_continuous(breaks = c(1,2,3,4,5,6,7,8),labels = c("<120","120-125","125-130","130-135","135-140(reference)","140-145","145-150",">150")) +
+  
+  geom_point(data = total_effect,aes(x=cat,y=(or-1)*100,colour="red"))+
+  geom_line(data = total_effect,aes(x=cat,y=(or-1)*100,colour="red"))+
+  geom_errorbar(data=total_effect
+                ,aes(x=cat,ymin=(or.lb-1)*100,ymax=(or.ub-1)*100)
+                ,alpha=0.3
+                ,inherit.aes=FALSE
+                ,width=0.2)+
+  scale_y_continuous(
+    position ="right",
+    
+    # Features of the first axis
+    name = "Proportion of mortality mediated by GCS score (%)",
+    
+    # Add a second axis and specify its features
+    sec.axis = sec_axis(~(./100)+1, name="Odds ratio (95%CI) of hospital mortality")
+  ) 
+dev.off()
+par(mfrow = c(1,1))
