5cfca8b0812bc5aadaefd247e45f7ab29f7cdd24
theonesp <ma.armengol.hcsc@gmail.com>
2024-01-19 16:46:39 +0100

    variables requested by reviewers adapted

--- analysis/sql/adm_dx.sql
+++ analysis/sql/adm_dx.sql
@@ -1,7 +1,23 @@
--- !preview conn=DBI::dbConnect(RSQLite::SQLite())
-
-  -- This query extracts (first available centered 12 hours on admit)
-  -- Admission 1x :
-  -- Admission sodium
+-- The ROW_NUMBER() function is used to assign a rank to each admitdxpath within each patientunitstayid based on their frequency.
+-- The PARTITION BY clause ensures that the ranking is done separately for each patientunitstayid.
+-- The WHERE clause filters out null or empty values in the admitdxpath.
+-- The COALESCE function is used to replace null or empty values with 'Other'.
+-- The final result selects the patientunitstayid and the top 5 ranked admitdxpath values for each patient.
   -- 
-SELECT patientunitstayid,admitdxpath FROM `physionet-data.eicu_crd.admissiondx`
+WITH TopAdmitSources AS (
+  SELECT
+    apacheadmissiondx,
+    COUNT(*) AS count_per_source
+  FROM `physionet-data.eicu_crd.patient`
+  WHERE apacheadmissiondx IS NOT NULL AND apacheadmissiondx <> ''
+  GROUP BY apacheadmissiondx
+  ORDER BY count_per_source DESC
+  LIMIT 5
+)
+SELECT
+  `physionet-data.eicu_crd.patient`.patientunitstayid,
+  COALESCE(TopAdmitSources.apacheadmissiondx, 'Other/Unknown') AS apacheadmissiondx
+FROM `physionet-data.eicu_crd.patient`
+LEFT JOIN TopAdmitSources ON `physionet-data.eicu_crd.patient`.apacheadmissiondx = TopAdmitSources.apacheadmissiondx
+WHERE `physionet-data.eicu_crd.patient`.apacheadmissiondx IS NOT NULL AND `physionet-data.eicu_crd.patient`.apacheadmissiondx <> '';
+
--- analysis/sql/adm_source.sql
+++ analysis/sql/adm_source.sql
@@ -1,7 +1,16 @@
--- !preview conn=DBI::dbConnect(RSQLite::SQLite())
-
-  -- This query extracts (first available centered 12 hours on admit)
-  -- Admission 1x :
-  -- Admission sodium
-  -- 
-select patientunitstayid,hospitaladmitsource FROM `physionet-data.eicu_crd.patient`
+WITH TopAdmitSources AS (
+  SELECT
+    hospitaladmitsource,
+    COUNT(*) AS count_per_source
+  FROM `physionet-data.eicu_crd.patient`
+  WHERE hospitaladmitsource IS NOT NULL AND hospitaladmitsource <> ''
+  GROUP BY hospitaladmitsource
+  ORDER BY count_per_source DESC
+  LIMIT 5
+)
+SELECT
+  p.patientunitstayid,
+  COALESCE(tas.hospitaladmitsource, 'Other/Unknown') AS adm_source
+FROM `physionet-data.eicu_crd.patient` p
+LEFT JOIN TopAdmitSources tas ON p.hospitaladmitsource = tas.hospitaladmitsource
+WHERE p.hospitaladmitsource IS NOT NULL AND p.hospitaladmitsource <> '';
